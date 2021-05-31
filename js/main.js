295aeadb17950cb1ab308b9697aaa0ab23c542c8
Nataly <cerise@inbox.ru>
2021-05-31 23:18:45 +0300

    Исправлены ошибки ESLint

--- js/main.js
+++ js/main.js
@@ -1,15 +1,21 @@
+//Функция возвращает случайное целое число
 function getRandomInteger(min, max) {
   if (min >= 0 && max > min) {
     const rand = min + Math.random() * (max + 1 - min);
     return Math.floor(rand);
   }
-  alert ('Ошибка');
+  return null;
 }
 
+getRandomInteger(3, 23);
+
+//Функция возвращает случайное число с плавающей точкой
 function getRandomFractional(min, max, length) {
   if (min >= 0 && max > min) {
     const rand = min + Math.random() * (max + 1 - min);
     return (+rand.toFixed(length));
   }
-  alert ('Ошибка');
+  return null;
 }
+
+getRandomFractional(20, 546, 7);
